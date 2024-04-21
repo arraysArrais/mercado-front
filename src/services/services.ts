7f02155d5b6be5cbe4360f73f7c30a85abdcf08f
arraysArrais <joaopedroarrais@gmail.com>
2024-04-21 18:17:43 -0300

    add service layer

---
+++ src/services/services.ts
@@ -0,0 +1,37 @@
+const baseUrl = import.meta.env.VITE_API_URL;
+
+const request = async (method: string, endpoint: string, params: any, token: string | null = null) => {
+    method = method.toLowerCase();
+    let fullUrl = baseUrl + endpoint;
+    let body = null;
+
+    if (method === 'get') {
+        let queryString = new URLSearchParams(params).toString();
+        fullUrl += '?' + queryString;
+    }
+    else {
+        body = JSON.stringify(params);
+    }
+
+    method = method.toUpperCase();
+    let headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    } as any;
+
+    if (token) {
+        headers['Authorization'] = 'Bearer ' + token
+    }
+
+    let req = await fetch(fullUrl, { method, headers, body });
+    return await req.json();
+}
+
+export default () => {
+    return {
+        //TODO: implemetar camada de interação com o backend  
+        getSomething: async () => {
+            return request('get', '/api/url', {}, localStorage.getItem('token'))
+        },
+    }
+}
